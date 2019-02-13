5de17083cadad415b37c2564732302ddf0a41553
Chris Butcher <c.butcher@hotmail.com>
2019-02-12 17:49:04 -0800

    Added salt and helper methods.

--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+const Crypto = require('crypto');
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
@@ -8,6 +9,7 @@ const userSchema = new Schema({
     email: String,
     username: String,
     password: String,
+    salt: String,
     created: { type: Date, default: Date.now },
     lastLogin: { type: Date, default: Date.now },
     enabled: Boolean,
@@ -43,6 +45,38 @@ userSchema.statics.findByEmailAndPassword = function(email, password, callback) 
         email: email,
         password: password
     }, callback);
+};
+
+/**
+ * Encrypt a plain-text password.
+ *
+ * @param {string} password
+ *
+ * @returns {string}
+ */
+userSchema.statics.encryptPassword = function (password, salt) {
+    return crypto.pbkdf2Sync(password, salt, 1000, 64, `sha512`).toString(`hex`);
+};
+
+/**
+ * Changes the users password.
+ *
+ * @param {string} password
+ */
+userSchema.methods.setPassword = function(password) {
+    this.salt = crypto.randomBytes(16).toString('hex');
+    this.password = this.encryptPassword(password, this.salt);
+};
+
+/**
+ * Check to see if the supplied password matches the users current password.
+ *
+ * @param {string} password
+ *
+ * @returns {boolean}
+ */
+userSchema.methods.validPassword = function(password) {
+    return this.password === this.encryptPassword(password, this.salt);
 };
 
 module.exports = mongoose.model('User', userSchema);
