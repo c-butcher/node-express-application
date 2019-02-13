549e12638a4e7bf9174567bd17343fa526eeec89
Chris Butcher <c.butcher@hotmail.com>
2019-02-12 17:55:54 -0800

    Updated login method so it looks up by the correct username/password fields.

--- routes/auth/local.js
+++ routes/auth/local.js
@@ -2,25 +2,42 @@ const express = require('express');
 const router = express.Router();
 const passport = require('passport');
 const LocalStrategy = require('passport-local').Strategy;
+const User = require('../../models/user');
 
 passport.use(new LocalStrategy({ usernameField: 'email', passwordField: 'password' }, (username, password, done) => {
     User.findOne({ username: username }, (err, user) => {
         if (err) { return done(err); }
 
         if (!user) {
-            return done(null, false, { message: 'Incorrect username.' });
+            User.findOne({ email: username }, (err, user) => {
+                if (err) { return done(err); }
+
+                if (!user) {
+                    return done(null, false, {message: 'Incorrect username or password.'});
+                }
+
+                if (!user.validPassword(password)) {
+                    return done(null, false, {message: 'Incorrect username or password.'});
+                }
+
+                return done(null, user);
+            });
         }
 
-        if (!user.validPassword(password)) {
-            return done(null, false, { message: 'Incorrect password.' });
-        }
+        else if (!user.validPassword(password)) {
+            return done(null, false, {message: 'Incorrect username or password.'});
 
-        return done(null, user);
+        } else {
+            return done(null, user);
+        }
     });
 }));
 
 router.get('/', (req, res, next) => {
-    res.render('auth/login');
+    res.render('auth/login', {
+        successes: req.flash('success'),
+        errors: req.flash('error'),
+    });
 });
 
 router.post('/', passport.authenticate('local', {
